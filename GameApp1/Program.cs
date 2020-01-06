3e4eefb386481541a94cfdb42a8684886953b485
Aaron Scalf <ascalf@J-LDimensional.com>
2020-01-06 16:08:07 -0600

    changed capitalization of user input, spacing of text, added a comment.

--- GameApp1/Program.cs
+++ GameApp1/Program.cs
@@ -14,31 +14,34 @@ namespace GameApp1
 
             while (true)
             {
-                Console.WriteLine("Dashing around a corner, away from The Beast, you see 2 doors." +
-                    "Which door do you choose? Type: Left or Right");
+                
+                Console.WriteLine("Dashing around a corner, away from The Beast, you see 2 doors. " +
+                    "Which door do you choose? Type: left or right");
 
                 var input = Console.ReadLine();
                 if (String.IsNullOrWhiteSpace(input))
                     break;
-                else if (input == "Left")
-                    Console.WriteLine("YOU ARE DEAD");
-                else if (input == "Right")
-                    Console.WriteLine("YOU GOT AWAY");
+                else if (input == "left")
+                    Console.WriteLine("You enter a long, torch lit hallway.");
+                else if (input == "right")
+                    Console.WriteLine("You enter a massive cathedral to The Dark Lord.");
+                //I'd like to make this dependant on your initial choice of Left or Right.
 
-                Console.WriteLine("Catching your breath, you see 2 weapons, an Axe and a Sword." +
+                Console.WriteLine("Catching your breath, you see 2 weapons, an Axe and a Sword. " +
                     "Type: Axe or Sword");
 
                 input = Console.ReadLine();
                 if (String.IsNullOrWhiteSpace(input))
                     break;
-                else if (input == "Axe")
-                    Console.WriteLine("Gimli's Power Axe, + 5 DMG");
-                else if (input == "Sword")
-                    Console.WriteLine("Aragorn's Sword of Light, + 5 DEX");
-                //comment
-                
-
+                else if (input == "axe")
+                    Console.WriteLine("Power Axe, + 5 DMG");
+                else if (input == "sword")
+                    Console.WriteLine("Sword of Speed, + 5 DEX");
+                                             
             }
+            
+            
+
 
             
             
