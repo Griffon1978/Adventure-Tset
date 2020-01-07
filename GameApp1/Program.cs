ba5d06912e5c2c530a8e58e5fe86d371d805164b
Aaron Scalf <ascalf@J-LDimensional.com>
2020-01-06 20:24:39 -0600

    started over. found a way better tutorial that will do what i want to do.

--- GameApp1/Program.cs
+++ GameApp1/Program.cs
@@ -1,50 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace GameApp1
 {
     class Program
     {
-        public static List<string> Left { get; private set; }
-        public static List<string> Right { get; private set; }
-
-        static void Main(string[] args)
+       static void Main()
         {
-            new List<string>();
+            string CharacterName = "John Doe";
 
-            while (true)
-            {
-                
-                Console.WriteLine("Dashing around a corner, away from The Beast, you see 2 doors. " +
-                    "Which door do you choose? Type: left or right");
-
-                var input = Console.ReadLine();
-                if (String.IsNullOrWhiteSpace(input))
-                    break;
-                else if (input == "left")
-                    Console.WriteLine("You enter a long, torch lit hallway.");
-                else if (input == "right")
-                    Console.WriteLine("You enter a massive cathedral to The Dark Lord.");
-                //I'd like to make this dependant on your initial choice of Left or Right.
-
-                Console.WriteLine("Catching your breath, you see 2 weapons, an Axe and a Sword. " +
-                    "Type: Axe or Sword");
-
-                input = Console.ReadLine();
-                if (String.IsNullOrWhiteSpace(input))
-                    break;
-                else if (input == "axe")
-                    Console.WriteLine("Power Axe, + 5 DMG");
-                else if (input == "sword")
-                    Console.WriteLine("Sword of Speed, + 5 DEX");
-                                             
-            }
-            
-            
-
-
-            
-            
+            Console.WriteLine("Game 1.1");
+            Console.WriteLine("Sunrise in the Underdark");
+            Console.WriteLine("Enter your Name: ");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Your Character is named " + CharacterName);
+            Console.ReadKey();
         }
 
     }
